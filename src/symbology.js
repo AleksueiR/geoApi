69580dc65c58068f6534c42c0a02ab8d126242f5
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-09-08 11:20:53 -0400

    _wipo_

--- src/symbology.js
+++ src/symbology.js
@@ -7,17 +7,10 @@ const svgjs = require('svg.js');
 // and deriving the appropriate image for a feature based on
 // a renderer
 
-// size of images to output
-const maxW = 32;
-const maxH = 32;
-
 // layer symbology types
 const SIMPLE = 'simple';
 const UNIQUE_VALUE = 'uniqueValue';
 const CLASS_BREAKS = 'classBreaks';
-
-// use single quotes so they will not be escaped (less space in browser)
-const emptySVG = `<svg xmlns='http://www.w3.org/2000/svg' width='32' height='32'></svg>`;
 
 /**
 * Will add extra properties to a renderer to support images.
@@ -182,320 +175,6 @@ function getGraphicSymbol(attributes, renderer) {
 }
 
 /**
-* Convert an ESRI colour object to SVG rgb format.
-* @private
-* @param  {Array} c ESRI Colour array
-* @return {String} colour in SVG format
-*/
-function colourToRgb(c) {
-    if (c) {
-        return `rgb(${c[0]},${c[1]},${c[2]})`;
-    } else {
-        return 'none';
-    }
-}
-
-/**
-* Convert an ESRI colour object to SVG opacity format.
-* @private
-* @param  {Array} c ESRI Colour array
-* @return {String} colour's opacity in SVG format
-*/
-function colourToOpacity(c) {
-    if (c) {
-        return c[3].toString();
-    } else {
-        return '0';
-    }
-}
-
-/**
-* Generate a utility object to help construct an SVG tag.
-* @private
-* @param  {String} type optional. the type of svg element (e.g. circle, path).
-* @return {String} colour in SVG format
-*/
-function newSVG(type) {
-    if (typeof type === 'undefined') {
-        type = '';
-    }
-    const mySVG = {
-        props: [],
-        type
-    };
-
-    // adds a property to the property collection
-    mySVG.addProp = (name, value) => {
-        mySVG.props.push({ name, value });
-    };
-
-    // output the svg tag as string
-    mySVG.belch = () => {
-
-        // construct tag with properties.
-        return `<${mySVG.type}` + mySVG.props.reduce((prev, curr) => {
-            return prev + ` ${curr.name}="${curr.value}"`;
-        }, '') + ' />';
-
-    };
-    return mySVG;
-}
-
-/**
-* Calculate the SVG fill for a symbol.
-* @private
-* @param  {Object} symbol a Simple ESRI symbol object.
-* @param  {Object} svg contains info on our SVG object (see newSVG). object is modified by the function
-*/
-function applyFill(symbol, svg) {
-
-    // NOTE: we cannot use ESRI simple fill with styles VERTICAL, HORIZONTAL, CROSS, DIAGONAL CROSS, FORWARD DIAGONAL, BACKWARD DIAGONAL
-    // ESRI implements these using image sprites containing the pattern, referenced in SVG using xlink tags.
-    // xlink is not supported in data URLs, which is what we are using.
-    // http://dbushell.com/2015/01/30/use-svg-part-2/
-
-    // possible awful fix: we draw our SVG to a canvas, then export the image as a data url there.
-
-    // second bad option: custom case, we have pre-made filled polygon (6 of them).  We would have to add the border (yuck)
-
-    // ok solution: add a second svg <path> with the hashes in it. just be lines. thin width, black, straight.  can adjust to size
-
-    // ------
-
-    // the none case will only apply to polygons. point symbols can only be empty fill via opacity
-    const fill = (symbol.type === 'esriSFS' && symbol.style !== 'esriSFSSolid') ? 'none' : colourToRgb(symbol.color);
-
-    svg.addProp('fill', fill);
-    svg.addProp('fill-opacity', colourToOpacity(symbol.color));
-    svg.addProp('fill-rule', 'evenodd');
-}
-
-/**
-* Calculate the SVG line style for a symbol.
-* @private
-* @param  {Object} lineSymbol a Simple ESRI symbol object.
-* @param  {Object} svg contains info on our SVG object (see newSVG). object is modified by the function
-*/
-function applyLine(lineSymbol, svg) {
-    if (lineSymbol) {
-        const stroke = lineSymbol.style === 'esriSLSNull' ? 'none' : colourToRgb(lineSymbol.color);
-
-        svg.addProp('stroke', stroke);
-        svg.addProp('stroke-opacity', colourToOpacity(lineSymbol.color));
-        svg.addProp('stroke-width', lineSymbol.width.toString());
-        svg.addProp('stroke-linecap', 'butt'); // huh huh
-        svg.addProp('stroke-linejoin', 'miter');
-        svg.addProp('stroke-miterlimit', '4');
-
-        const ESRI_DASH_MAPS = {
-            esriSLSSolid: 'none',
-            esriSLSDash: '5.333,4',
-            esriSLSDashDot: '5.333,4,1.333,4',
-            esriSLSLongDashDotDot: '10.666,4,1.333,4,1.333,4',
-            esriSLSDot: '1.333,4',
-            esriSLSLongDash: '10.666,4',
-            esriSLSLongDashDot: '10.666,4,1.333,4',
-            esriSLSShortDash: '5.333,1.333',
-            esriSLSShortDashDot: '5.333,1.333,1.333,1.333',
-            esriSLSShortDashDotDot: '5.333,1.333,1.333,1.333,1.333,1.333',
-            esriSLSShortDot: '1.333,1.333',
-            esriSLSNull: 'none'
-        };
-
-        svg.addProp('stroke-dasharray', ESRI_DASH_MAPS[lineSymbol.style]);
-    } else {
-        // odd case where a symbol is missing an outline, so an undefined gets passed in
-        svg.addProp('stroke', 'none');
-    }
-}
-
-/**
-* Calculate the SVG rotation for a symbol.
-* @private
-* @param  {Object} symbol a Simple ESRI symbol object.
-* @param  {Object} svg contains info on our SVG object (see newSVG). object is modified by the function
-*/
-function applyRotation(symbol, svg) {
-
-    // https://sarasoueidan.com/blog/svg-transformations/
-
-    /*
-    const toRad = ang => ang * (Math.PI / 180);
-    const cos = Math.cos(toRad(angle));
-    const sin = Math.sin(toRad(angle));
-    // `matrix(${cos},${sin},${-sin},${cos},0,0)`);
-    */
-
-    const angle = symbol.angle || 0;
-    svg.addProp('transform', `rotate(${angle},${maxW / 2},${maxH / 2})`);
-
-}
-
-/**
-* Generate an SVG object for a circle marker symbol.
-* @private
-* @param  {Object} symbol a SimpleMarker ESRI symbol object, circle style.
-* @return {Object} SVG object with circle-specific definitions
-*/
-function makeCircleSVG(symbol) {
-    const circleSVG = newSVG('circle');
-
-    // radius. size is diameter. cap at max image size
-    circleSVG.addProp('r', Math.min(symbol.size / 2, (maxW - 4) / 2).toString());
-
-    // center circle
-    circleSVG.addProp('cx', (maxW / 2).toString());
-    circleSVG.addProp('cy', (maxH / 2).toString());
-
-    return circleSVG;
-}
-
-/**
-* Calculate boundaries for drawing non-circle markers. Will cap boundaries at max image size.
-* Assumes square image
-* @private
-* @param  {Number} size the size of the marker.
-* @return {Object} object containing upper left (.upLeft), lower right (.loRite) and middle (.middle) boundaries
-*/
-function getGlyphCorners(size) {
-    // if marker is too big, make it fit
-    const trimSize = Math.min(size, maxW - 4);
-
-    const offset = trimSize / 2;
-    const middle = maxW / 2;
-    return {
-        upLeft: middle - offset,
-        loRite: middle + offset,
-        middle
-    };
-}
-
-/**
-* Generate an SVG object for a non-circle marker symbol.
-* @private
-* @param  {Object} symbol a SimpleMarker ESRI symbol object, non-circle style.
-* @return {Object} SVG object with marker definitions
-*/
-function makeGlyphSVG(symbol) {
-    const glyphSVG = newSVG('path');
-    let path;
-
-    // get the appropriate drawing path for the symbol
-    if (symbol.style === 'esriSMSPath') {
-        path = symbol.path;
-    } else {
-        // jscs:disable maximumLineLength
-        const c = getGlyphCorners(symbol.size);
-        switch (symbol.style) {
-            case 'esriSMSCross':
-                path = `M ${c.upLeft},${c.middle} ${c.loRite},${c.middle} M ${c.middle},${c.loRite} ${c.middle},${c.upLeft}`;
-                break;
-            case 'esriSMSDiamond':
-                path = `M ${c.upLeft},${c.middle} ${c.middle},${c.loRite} ${c.loRite},${c.middle} ${c.middle},${c.upLeft} Z`;
-                break;
-            case 'esriSMSSquare':
-                path = `M ${c.upLeft},${c.upLeft} ${c.upLeft},${c.loRite} ${c.loRite},${c.loRite} ${c.loRite},${c.upLeft} Z`;
-                break;
-            case 'esriSMSX':
-                path = `M ${c.upLeft},${c.upLeft} ${c.loRite},${c.loRite} M ${c.upLeft},${c.loRite} ${c.loRite},${c.upLeft}`;
-                break;
-            case 'esriSMSTriangle':
-                path = `M ${c.upLeft},${c.loRite} ${c.middle},${c.upLeft} ${c.loRite},${c.loRite} Z`;
-                break;
-        }
-
-        // jscs:enable maximumLineLength
-    }
-
-    glyphSVG.addProp('d', path);
-    return glyphSVG;
-}
-
-/**
-* Generate an SVG object for a simple marker symbol.
-* @private
-* @param  {Object} symbol a SimpleMarker ESRI symbol object
-* @return {Object} SVG object with marker definitions
-*/
-function makeMarkerSVG(symbol) {
-    let svg;
-
-    if (symbol.style === 'esriSMSCircle') {
-        svg = makeCircleSVG(symbol);
-    } else {
-        svg = makeGlyphSVG(symbol);
-    }
-
-    applyLine(symbol.outline, svg);
-    applyFill(symbol, svg);
-    applyRotation(symbol, svg);
-
-    return svg;
-
-}
-
-/**
-* Generate an SVG object for a simple fill symbol.
-* @private
-* @param  {Object} symbol a SimpleFill ESRI symbol object
-* @return {Object} SVG object with fill definitions
-*/
-function makePolySVG(symbol) {
-    const polySVG = newSVG('rect');
-
-    polySVG.addProp('x', '4');
-    polySVG.addProp('y', '4');
-    polySVG.addProp('width', (maxW - 8).toString());
-    polySVG.addProp('height', (maxH - 8).toString());
-    applyFill(symbol, polySVG);
-    applyLine(symbol.outline, polySVG);
-
-    return polySVG;
-}
-
-/**
-* Generate an SVG object for a simple line symbol.
-* @private
-* @param  {Object} symbol a SimpleLine ESRI symbol object
-* @return {Object} SVG object with line definitions
-*/
-function makeLineSVG(symbol) {
-    const lineSVG = newSVG('path');
-
-    // diagonal line
-    lineSVG.addProp('d', `M 4,4 ${maxW - 4},${maxH - 4}`);
-
-    applyLine(symbol, lineSVG);
-
-    return lineSVG;
-}
-
-/**
-* Generate an SVG definition for a simple symbol.
-* @private
-* @param  {Object} symbol a Simple ESRI symbol object
-* @return {String} symbol svg as text
-*/
-function makeSVG(symbol) {
-
-    const head = `<svg xmlns="http://www.w3.org/2000/svg" width="${maxW}" height="${maxH}">`;
-    const foot = '</svg>';
-
-    const typeHandler = {
-        esriSMS: makeMarkerSVG,
-        esriSLS: makeLineSVG,
-        esriCLS: makeLineSVG,
-        esriSFS: makePolySVG
-    };
-
-    const svg = typeHandler[symbol.type](symbol);
-
-    // use single quotes so they will not be escaped (less space in browser)
-    return (head + svg.belch() +  foot).replace(/"/g, `'`);
-
-}
-
-/**
 * Generate a legend item for an ESRI symbol.
 * @private
 * @param  {Object} symbol an ESRI symbol object in server format
@@ -546,6 +225,7 @@ function symbolToLegend(symbol, label, window) {
 
     // jscs:enable requireSpacesInAnonymousFunctionExpression
 
+    // line dash styles
     const ESRI_DASH_MAPS = {
         esriSLSSolid: 'none',
         esriSLSDash: '5.333,4',
@@ -561,6 +241,7 @@ function symbolToLegend(symbol, label, window) {
         esriSLSNull: 'none'
     };
 
+    // default stroke style
     const DEFAULT_STROKE = {
         color: '#000',
         opacity: 1,
@@ -636,7 +317,7 @@ function symbolToLegend(symbol, label, window) {
     // jscs doesn't like enhanced object notation
     // jscs:disable requireSpacesInAnonymousFunctionExpression
     const symbolTypes = {
-        esriSMS() {
+        esriSMS() { // ESRI Simple Marker Symbol
             const symbolColour = parseEsriColour(symbol.color);
 
             const outlineColour = parseEsriColour(symbol.outline.color);
@@ -659,7 +340,7 @@ function symbolToLegend(symbol, label, window) {
 
             fitInto(marker, contentSize);
         },
-        esriSLS() {
+        esriSLS() { // ESRI Simple Line Symbol
             const lineColour = parseEsriColour(symbol.color);
             const lineStroke = makeStroke({
                     color: lineColour.colour,
@@ -672,10 +353,10 @@ function symbolToLegend(symbol, label, window) {
             draw.line(contentPadding, contentPadding, containerSize - contentPadding, containerSize - contentPadding)
                 .stroke(lineStroke);
         },
-        esriCLS() {
+        esriCLS() {  // ESRI Fancy Line Symbol
             this.esriSLS();
         },
-        esriSFS() {
+        esriSFS() { // ESRI Simple Fill Symbol
             const symbolColour = parseEsriColour(symbol.color);
             const symbolStroke = makeStroke({
                 color: symbolColour.colour,
@@ -703,8 +384,6 @@ function symbolToLegend(symbol, label, window) {
         },
 
         esriPFS() { // ESRI Picture Fill Symbol
-            // console.error('no support for feature service legend picture fill');
-
             // imageUri can be just an image url is specified or a dataUri string
             const imageUri = symbol.imageData ? `data:${symbol.contentType};base64,${symbol.imageData}` : symbol.url;
 
@@ -753,6 +432,33 @@ function symbolToLegend(symbol, label, window) {
 
     // jscs:enable requireSpacesInAnonymousFunctionExpression
 
+    // console.log(symbol.type, label, '--START--');
+    // console.log(symbol);
+
+    return Promise.resolve(symbolTypes[symbol.type]())
+        .then(() => {
+            // console.log(symbol.type, label, '--DONE--');
+
+            // remove element from the page
+            window.document.body.removeChild(container);
+            return { label, svgcode: draw.svg() };
+        }).catch(error => console.log(error));
+
+    /**
+     * Creates a stroke style by applying custom rules to the default stroke.
+     * @param {Object} overrides any custom rules to apply on top of the defaults
+     * @return {Object} a stroke object
+     * @private
+     */
+    function makeStroke(overrides) {
+        return Object.assign({}, DEFAULT_STROKE, overrides);
+    }
+
+    /**
+     * Fits svg element in the size specified
+     * @param {Ojbect} element svg element to fit
+     * @param {Number} containerSize width/height of a container to fit the element into
+     */
     function fitInto(element, containerSize) {
         // const elementRbox = element.rbox();
         // const elementRbox = element.screenBBox();
@@ -764,22 +470,12 @@ function symbolToLegend(symbol, label, window) {
         }
     }
 
-    console.log(symbol.type, label, '--START--');
-    console.log(symbol);
-
-    return Promise.resolve(symbolTypes[symbol.type]())
-        .then(() => {
-            console.log(symbol.type, label, '--DONE--');
-
-            // remove element from the page
-            window.document.body.removeChild(container);
-            return { label, svgcode: draw.svg() };
-        }).catch(error => console.log(error));
-
-    function makeStroke(overrides) {
-        return Object.assign({}, DEFAULT_STROKE, overrides);
-    }
-
+    /**
+    * Convert an ESRI colour object to SVG rgb format.
+    * @private
+    * @param  {Array} c ESRI Colour array
+    * @return {Object} colour and opacity in SVG format
+    */
     function parseEsriColour(c) {
         if (c) {
             return {
@@ -892,7 +588,7 @@ function getZoomLevel(lods, maxScale) {
     return currentLod;
 }
 
-module.exports = function (window) {
+module.exports = window => {
     return {
         getGraphicIcon,
         getGraphicSymbol,
